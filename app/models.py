03c3bcafa4990562049119944f46308a4de49c90
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 20:16:40 +0300

    create image class and migrate

--- app/models.py
+++ app/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    # profile_photo = models.ImageField(upload_to = '/profile', default = 'default.jpg')
+    profile_photo = models.ImageField(upload_to = 'profile/', default = 'default.jpg')
     profile_bio = models.CharField(max_length = 100)
     profile_user = models.OneToOneField(User, on_delete = models.CASCADE)
+    
+    
+    
+class Image(models.Model):
+    image_path = models.ImageField(upload_to = 'posts/')
+    image_name = models.CharField(max_length = 50)
+    image_caption = models.TextField(max_length = 300)
+    image_profile = models.ForeignKey(Profile, null = True, on_delete = models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
