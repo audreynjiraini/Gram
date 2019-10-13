254696509266a1f781184271cb71836ae0d0b522
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 21:44:40 +0300

    create setup and is instance test for Image class

--- app/tests.py
+++ app/tests.py
@@ -44,3 +44,25 @@ class ProfileTestClass(TestCase):
         
         self.audrey = Profile.objects.get(profile_bio='Test 2')
         self.assertEqual(self.audrey.profile_bio, 'Test 2')
+        
+        
+        
+class ImageTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.user = User(username = 'audrey', email = 'audreywncode@gmail.com', password = 'njiraini123')
+        self.user.save()
+        
+        self.audrey = Profile(id = 50, profile_photo = 'default.jpg', profile_bio = 'Test 1', profile_user = self.user)
+        self.audrey.save()
+        
+        self.image = Image(image_path = 'image.jpg', image_name = 'Test',image_caption = 'Test 1', image_profile = self.audrey, pub_date = '2019-10-10')
+        
+        
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+    
+    
+    # Testing Save Method
