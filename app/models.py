0ae1ba04994a802e6679b4baa9e7fc69475951ad
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 21:30:09 +0300

    create save, delete and update methods for Image class

--- app/models.py
+++ app/models.py
@@ -32,3 +32,19 @@ class Image(models.Model):
     image_caption = models.TextField(max_length = 300)
     image_profile = models.ForeignKey(Profile, null = True, on_delete = models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add = True)
+    
+    
+    def __str__(self):
+        return self.image_caption
+    
+    
+    def save_image(self):
+        self.save()
+        
+        
+    def delete_image(self):
+        self.delete()
+        
+        
+    def update_caption(self):
+        Image.objects.filter(pk = id).update(image_caption = image_caption)
