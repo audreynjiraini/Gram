b84f6ebb2e6effa2face42b329bd95eac076a4f3
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 23:30:25 +0300

    import tinymce and create NewProfile and NewPosts forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from .models import Profile, Image
 
 
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label = 'First Name', max_length = 30)
     email = forms.EmailField(label = 'Email')
+    
+    
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+
+
+class NewPostsForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['pub_date','image_profile']
