3411043524bb84c73451a1e179ca6aaab3fc1289
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 20:08:08 +0300

    edit profile class and change settings to allow for uploading images

--- app/models.py
+++ app/models.py
@@ -5,4 +5,5 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     # profile_photo = models.ImageField(upload_to = '/profile', default = 'default.jpg')
-    bio = models.CharField(max_length = 100)
+    profile_bio = models.CharField(max_length = 100)
+    profile_user = models.OneToOneField(User, on_delete = models.CASCADE)
