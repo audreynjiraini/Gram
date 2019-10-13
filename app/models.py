25ebc5b1e99d828b7930a219717129e4405df2df
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 19:55:40 +0300

    create profile model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    # profile_photo = models.ImageField(upload_to = '/profile', default = 'default.jpg')
+    bio = models.CharField(max_length = 100)
