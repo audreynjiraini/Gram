2a7971f4bb316596abcf569aa2e5ecfad0887c16
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-13 18:38:50 +0300

    register app in settings and create index template

--- app/views.py
+++ app/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def welcome(request):
-    return HttpResponse('Welcome to Gram')
+def index(request):
+    return render(request, 'index.html')
