38a989409431e018acf8ec9254bd61bfe0db0dae
Audrey Wanjiku <audreynjiraini@gmail.com>
2019-10-14 18:38:49 +0300

    create myprofile html template

--- app/urls.py
+++ app/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^register/', views.register, name='register'),
     url('^$', views.index, name = 'index'),
-    url(r'^create/profile$', views.create_profile, name = 'create-profile'),
+    url(r'^profile/$', views.create_profile, name = 'create-profile'),
     url(r'^post/', views.new_post, name = 'new-post'),
     url(r'^home/$', views.home, name = 'home'),
     url(r'^search/', views.search_results, name = 'search_results'),
